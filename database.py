fa41fb00018f2ce8b357570b1f3d5d95a6eba9d5
Isaacayomi <okunlolaa3@outlook.com>
2026-03-23 15:27:38 +0100

    connect local-db to neon db

--- database.py
+++ database.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()  # loads variables from .env
 
 def get_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT")
-    )
+    # return psycopg2.connect(
+    #     host=os.getenv("DB_HOST"),
+    #     database=os.getenv("DB_NAME"),
+    #     user=os.getenv("DB_USER"),
+    #     password=os.getenv("DB_PASSWORD"),
+    #     port=os.getenv("DB_PORT")
+    # )
+    return psycopg2.connect(os.getenv("DB_URL"))
